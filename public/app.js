7551df03cd782c9ea4e619f12fd867032e571b87
John Daly <jpdaly@gmail.com>
2017-03-29 20:05:49 +0100

    set background image, some css tweaks

--- public/app.js
+++ public/app.js
@@ -17,6 +17,10 @@ var requestComplete = function(){
   populateList(JSONobject);
 };
 
+var getRandomAlbumFromList = function(array){
+  return array[Math.floor(Math.random() * array.length)]
+}
+
 var populateList = function(object){
   var listOfAlbums = object.albums.items
   console.log(listOfAlbums);
@@ -25,6 +29,8 @@ var populateList = function(object){
   var table = document.createElement('table');
   var tableHead = document.createElement('thead');
   var tableBody = document.createElement('tbody');
+  
+  var sleeveHead = document.createElement('th');
   var artistHead = document.createElement('th');
   artistHead.innerText = 'Artist';
   var albumTitleHead = document.createElement('th');
@@ -32,23 +38,36 @@ var populateList = function(object){
 
   albumListDiv.appendChild(table);
   table.appendChild(tableHead);
+  tableHead.appendChild(sleeveHead);
   tableHead.appendChild(artistHead);
   tableHead.appendChild(albumTitleHead);
   table.appendChild(tableBody);
 
 
   listOfAlbums.forEach(function(album){
+    // create a new row and attach to tbody
     var tr = document.createElement('tr');
     tableBody.appendChild(tr);
+    // create a cell for sleeve and add image
+    var sleeve = document.createElement('td');
+    sleeve.classList.add('sleeve');
+    var sleeveImage = document.createElement('img');
+    sleeveImage.setAttribute('src', album.images[0].url);
+    sleeve.appendChild(sleeveImage);
+    // create cell for artist name and populate
     var artistName = document.createElement('td');
     artistName.innerText = album.artists[0].name;
+    // create cell for title and populate
     var albumTitle = document.createElement('td');
     albumTitle.innerText = album.name;
+    // add 3 cells elements to the new row
+    tr.appendChild(sleeve);
     tr.appendChild(artistName);
     tr.appendChild(albumTitle);
 
-
-  })
+    var body = document.querySelector('body');
+    body.setAttribute('background', getRandomAlbumFromList(listOfAlbums).images[0].url)
+  });
 }
 
 var app = function(){
